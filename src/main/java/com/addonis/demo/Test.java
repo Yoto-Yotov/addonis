f010bbad720eb8ba13b88d90b13120665a644958
yoto741 <yoto721@abv.bg>
2020-04-06 12:16:33 +0300

    Add SwaggerConfig and application.properties DB credentials

--- src/main/java/com/addonis/demo/Test.java
+++ src/main/java/com/addonis/demo/Test.java
@@ -14,18 +14,19 @@ public class Test {
 
         if (json instanceof JSONArray) {
             JSONArray arr = (JSONArray) json;
-            for (int i = 0; i < arr.length(); i++) {
+          //  for (int i = 0; i < arr.length(); i++) {
 //                //long id = j.getLong("id");
 //                //JSONObject owner = j.getJSONObject("owner");
 //                //System.out.println(id);
-                JSONObject j = (JSONObject) arr.get(i);
+                JSONObject j = (JSONObject) arr.get(0);
                 System.out.println(j);
-            }
-            JSONObject j = (JSONObject) arr.get(0);
+            //}
+            //JSONObject j = (JSONObject) arr.get(0);
             System.out.println(j);
             System.out.println(j.getJSONObject("commit").getString("message").replaceAll("\n", " "));
             System.out.println(j.getJSONObject("commit").getJSONObject("author").getString("date"));
             System.out.println("Size = " +  arr.length());
+
         } else {
             System.out.println(json);
         }
