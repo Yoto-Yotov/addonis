d44ea97801be74a605828109722210ae5ac69c68
Liliya Burlakova <lhlhya@gmail.com>
2020-04-10 14:02:35 +0300

    Add RestTagController

---
+++ src/main/java/com/addonis/demo/controllers/rest/RestTagController.java
@@ -0,0 +1,16 @@
+package com.addonis.demo.controllers.rest;
+
+import com.addonis.demo.services.contracts.TagService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/tag")
+public class RestTagController {
+
+    private TagService tagService;
+    
+    public RestTagController(TagService tagService) {
+        this.tagService = tagService;
+    }
+}
