2ff9f03e56e3ece1ae19e9b4a4ed616cd7ba6f0a
Liliya Burlakova <lhlhya@gmail.com>
2020-04-12 16:39:52 +0300

    Add @Autowired in LastCommitService

--- src/main/java/com/addonis/demo/services/LastCommitServiceImpl.java
+++ src/main/java/com/addonis/demo/services/LastCommitServiceImpl.java
@@ -3,6 +3,7 @@ package com.addonis.demo.services;
 import com.addonis.demo.models.LastCommit;
 import com.addonis.demo.repository.contracts.LastCommitRepository;
 import com.addonis.demo.services.contracts.LastCommitService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public class LastCommitServiceImpl implements LastCommitService {
 
     private LastCommitRepository lastCommitRepository;
 
+    @Autowired
     public LastCommitServiceImpl(LastCommitRepository lastCommitRepository) {
         this.lastCommitRepository = lastCommitRepository;
     }
