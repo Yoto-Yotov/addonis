ca2b86902b8b3e2e653fd350086415a1704168be
yoto741 <yoto721@abv.bg>
2020-04-13 19:16:12 +0300

    remove unneccesary service from RestTagController

--- src/main/java/com/addonis/demo/controllers/rest/RestTagController.java
+++ src/main/java/com/addonis/demo/controllers/rest/RestTagController.java
@@ -17,7 +17,6 @@ import java.util.List;
 public class RestTagController {
 
     private TagService tagService;
-    private AddonService addonService;
 
     @Autowired
     public RestTagController(TagService tagService) {
