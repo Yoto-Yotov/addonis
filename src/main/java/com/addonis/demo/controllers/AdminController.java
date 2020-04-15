f51a7f5bcbcdaff8aa632b47a400a7a9535e937c
Liliya Burlakova <lhlhya@gmail.com>
2020-04-16 00:07:22 +0300

    Add user disable method

--- src/main/java/com/addonis/demo/controllers/AdminController.java
+++ src/main/java/com/addonis/demo/controllers/AdminController.java
@@ -1,10 +1,12 @@
 package com.addonis.demo.controllers;
 
+import com.addonis.demo.models.UserInfo;
 import com.addonis.demo.services.contracts.AddonService;
 import com.addonis.demo.services.contracts.UserInfoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.security.Principal;
 
@@ -30,6 +32,14 @@ public class AdminController {
     @GetMapping("/admin/users")
     public String showUsersToPortal(Model model, Principal principal) {
         model.addAttribute("users", userInfoService.getAll());
+        return "users";
+    }
+
+    @PostMapping("/admin/users/d")
+    public String disableUser(Model model, Principal principal) {
+
+        UserInfo userToDel = userInfoService.gerUserByUsername(principal.getName());
+        userInfoService.deleteById(userToDel.getId());
         return "users";
     }
 }
