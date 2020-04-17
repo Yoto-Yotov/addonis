7d5f75d096ca66fd2b9f37fabada473778652447
yoto741 <yoto721@abv.bg>
2020-04-17 11:31:52 +0300

    Fix mixed case problem

--- src/main/java/com/addonis/demo/repository/contracts/RatingRepository.java
+++ src/main/java/com/addonis/demo/repository/contracts/RatingRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RatingRepository extends BaseRepository<Rating, Integer> {
 
-    Rating getByUserInfo_IdAndAddon_Id(int userId, int addonId);
+    Rating getByUserInfoIdAndAddonId(int userId, int addonId);
 
     @Query(value = "SELECT IFNULL(AVG(rating), 0) FROM rating WHERE addon_id = :addon_id", nativeQuery = true)
     double getAverageRating(@Param("addon_id") int addonId);
