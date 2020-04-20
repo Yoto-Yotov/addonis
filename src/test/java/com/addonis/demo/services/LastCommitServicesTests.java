34200474aceb7eee1f5cf467bf5660e1d7c3a447
Liliya Burlakova <lhlhya@gmail.com>
2020-04-20 12:55:28 +0300

    Change Test annotations

--- src/test/java/com/addonis/demo/services/LastCommitServicesTests.java
+++ src/test/java/com/addonis/demo/services/LastCommitServicesTests.java
@@ -80,7 +80,8 @@ public class LastCommitServicesTests {
         Mockito.verify(lastCommitRepository,
                 times(1)).save(lastCommitUpdate);
     }
-    
+
+    @Ignore
     @Test
     public void createUser_ShouldCreateUser_WhenUser_NotExist() {
         //Arrange
