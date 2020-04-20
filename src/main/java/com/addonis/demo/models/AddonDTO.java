b33c4210ec28106d64016b8f72d8af8739945c7b
Liliya Burlakova <lhlhya@gmail.com>
2020-04-20 13:13:42 +0300

    Add AddonDto

---
+++ src/main/java/com/addonis/demo/models/AddonDTO.java
@@ -0,0 +1,29 @@
+package com.addonis.demo.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@NoArgsConstructor
+public class AddonDTO {
+
+    @NotNull
+    @NotBlank
+    @Size(min = 2, max = 50, message = "Addon name length should be between 2 and 50 symbols")
+    private String name;
+
+    @NotNull
+    @NotBlank
+    private String description;
+
+
+    private UserInfo creator;
+
+    @NotNull
+    @NotBlank
+    private String link;
+}
