ee39d37d472b09af320d8eacbbb51a37e9062dbc
yoto741 <yoto721@abv.bg>
2020-04-23 14:43:22 +0300

    Refactor the home page and add about us page
    Remove unneded element of the home.html. Add about.html

--- src/main/java/com/addonis/demo/repository/contracts/AddonRepository.java
+++ src/main/java/com/addonis/demo/repository/contracts/AddonRepository.java
@@ -1,7 +1,6 @@
 package com.addonis.demo.repository.contracts;
 
 import com.addonis.demo.models.Addon;
-import com.addonis.demo.models.UserInfo;
 import com.addonis.demo.repository.base.BaseRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
