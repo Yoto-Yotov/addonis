0d81359567b6ce7ba754bdf8dc492bbbc6e157b3
Yoto Vasilev Yotov <yoto721@abv.bg>
2020-05-01 07:21:04 +0000

    Update AddonsController.java

--- src/main/java/com/addonis/demo/controllers/AddonsController.java
+++ src/main/java/com/addonis/demo/controllers/AddonsController.java
@@ -265,9 +265,7 @@ public class AddonsController {
 
     @GetMapping("/addons/ide/{ideName}")
     public String getAllFilterByIde(@PathVariable("ideName") String ideName, Model model) {
-
         model.addAttribute("addons", addonService.getAllFilterByIdeName(ideName));
-
         model.addAttribute("tags", tagService.getAll());
         model.addAttribute("ides", ideService.getAll());
 
@@ -276,9 +274,7 @@ public class AddonsController {
 
     @GetMapping("/addons/tag/{tagName}")
     public String getAllFilterByTag(@PathVariable("tagName") String tagName, Model model) {
-
         model.addAttribute("addons", addonService.getAllFilterByTagName(tagName));
-
         model.addAttribute("tags", tagService.getAll());
         model.addAttribute("ides", ideService.getAll());
 
